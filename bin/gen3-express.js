f432dce29e47b9e76a742366dfee283da4bc7d84
hαrsh vαirαgi <harshb7839@gmail.com>
2024-09-21 01:12:45 +0530

    tui has been implemented to gen3-express.
    - used signale to implement tui.
    - changes many things in gen3-express file.
    - renamed README file to readme.

--- bin/gen3-express.js
+++ bin/gen3-express.js
@@ -1,32 +1,56 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
 const path = require('path')
 const fse = require('fs-extra')
+const signale = require('signale')
 const { program } = require('commander')
 
-const templatePath = path.resolve(__dirname, '../template')
+// path of template.
+const template = path.resolve(__dirname, '../template')
 
-// function to copy template files.
-const createApp = async (appName) => {
-  const destPath = path.resolve(process.cwd(), appName)
+// change package name of a node package.
+// @param {string} name - new name of node package.
+// @param {string} path - path of package file node package.
+const change_package = (name, path) => {
   try {
-    await fse.copy(templatePath, destPath)
-    console.log(`[+] Express app has been created at ${destPath}...`)
+    const package_data = require(path)
+    package_data.name = name
+    fs.writeFileSync(path, JSON.stringify(package_data, null, 2))
   } catch (error) {
-    console.error(`[-] ${error.message}`)
+    signale.error(new Error('Apologize for failed to write package file.'));
   }
 }
 
+// function to copy template files.
+async function create_app(app_name) {
+  const destination = path.resolve(process.cwd(), app_name)
+  try {
+    await fse.copy(template, destination)
+    signale.success(`Copying template files to ${app_name} is successful.`)
+    change_package(app_name, `${destination}/package.json`)
+    signale.success(`Resolving name of package is successful.`)
+  } catch (error) {
+    signale.error(error)
+  }
+}
+
+// set arguments and actions to create app.
 program
   .version('1.0.0')
-  .argument('<app-name>', 'Name of the application')
-  .action(async (appName) => {
+  .argument('<app-name>', 'name to create new app.')
+  .action(async (app_name) => {
+    signale.time()
     // check if folder already exist.
-    if (fse.existsSync(appName)) {
-      console.error('[-] Express is app already exist...')
-      return
+    if (!fse.existsSync(app_name)) {
+      signale.await(`${app_name} app is on the way.`)
+      await create_app(app_name)
+      signale.complete(`A new app has been created as ${app_name}.`)
+    } else {
+      signale.error(`${app_name} app does exist in same folder.`)
     }
-    await createApp(appName)
+    signale.timeEnd()
   })
 
+// exection of process.
 program.parse(process.argv)
